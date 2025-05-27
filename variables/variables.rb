1cae6c725d392f5234d2d9d3eb2fa7418b01fe19
pa-deasy <pa-deasy@hotmail.com>
2025-05-27 14:15:50 -0700

    Variables examples.

---
+++ variables/variables.rb
@@ -0,0 +1,17 @@
+age = 18
+puts "#{age}"
+age = 18 + 5
+puts "#{age}"
+age = age + 2
+puts "#{age}"
+age += 5
+puts "#{age}"
+
+desired_location = "Barcelona"
+johns_location = desired_location
+puts "desired_location is #{desired_location}"
+puts "johns_location is #{johns_location}"
+
+desired_location.upcase!
+puts "desired_location is #{desired_location}"
+puts "johns_location is #{johns_location}"
