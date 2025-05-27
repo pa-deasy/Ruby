f2db6f224c4eba9f7f88e4449ccad313423ec684
pa-deasy <pa-deasy@hotmail.com>
2025-05-27 14:15:43 -0700

    Input and output examples.

---
+++ input_and_output/input.rb
@@ -0,0 +1,5 @@
+input = gets
+puts input
+
+no_separators = gets.chomp
+puts no_separators
---
+++ input_and_output/output.rb
@@ -0,0 +1,17 @@
+print "Learning to code is FUN!"
+print "1234"
+
+puts "Learning to code is COOL!"
+
+print "Hello world"
+print "I love drinking coffe"
+puts "\nvs"
+puts "Hello world"
+puts "I love drinking coffe"
+
+# p is for debugging
+p "Hello there"
+pp "Hello there"
+
+p [1, 2, 3, 4]
+pp [1, 2, 3, 4]
